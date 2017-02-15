27bb9988254bf412c476128cdbea58edf24a4061
BedivereZero <BedivereZero@gmail.com>
2017-02-15 16:02:18 +0800

    define class: Snake, Food, Demo

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+
+
+class Snake(object):
+    """snake"""
+    def move():
+        pass
+
+    def eat():
+        pass
+
+
+class Food(object):
+    """food"""
+    pass
+
+class Demo(object):
+    """pass"""
