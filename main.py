74f1f635a7ef2ee96b4774caced7553b13112b2b
BedivereZero <BedivereZero@gmail.com>
2017-02-27 16:20:44 +0800

    define exceptions

--- main.py
+++ main.py
@@ -2,6 +2,24 @@
 # -*- coding:UTF-8 -*-
 
 
+class GoodEnding(Exception):
+    """good ending"""
+    pass
+
+
+class BadEnding(Exception):
+    """bad ending"""
+    pass
+
+
+class EatSelf(Exception):
+    pass
+
+
+class KnockBorder(Exception):
+    pass
+
+
 class Snake(object):
     """snake"""
     def move():
@@ -15,5 +33,7 @@ class Food(object):
     """food"""
     pass
 
+
 class Demo(object):
     """pass"""
+    pass
